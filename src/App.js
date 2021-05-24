1daf2fb154bb42847c933ee87d0b507e8f210975
yhnu <buutuud@gmail.com>
2021-05-24 09:20:18 +0800

    关闭通知和修改CreatedBy

--- src/App.js
+++ src/App.js
@@ -28,40 +28,40 @@ class App extends Component {
     };
   }
   componentDidMount() {
-    const openNotification = () => {
-      notification.open({
-        message: "博主-yezihaohao",
-        description: (
-          <div>
-            <p>
-              GitHub地址：{" "}
-              <a
-                href="https://github.com/yezihaohao"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                https://github.com/yezihaohao
-              </a>
-            </p>
-            <p>
-              博客地址：{" "}
-              <a
-                href="https://yezihaohao.github.io/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                https://yezihaohao.github.io/
-              </a>
-            </p>
-          </div>
-        ),
-        icon: <Icon type="smile-circle" style={{ color: "red" }} />,
-        duration: 0
-      });
-      localStorage.setItem("isFirst", JSON.stringify(true));
-    };
-    const isFirst = JSON.parse(localStorage.getItem("isFirst"));
-    !isFirst && openNotification();
+    // const openNotification = () => {
+    //   notification.open({
+    //     message: "博主-yezihaohao",
+    //     description: (
+    //       <div>
+    //         <p>
+    //           GitHub地址：{" "}
+    //           <a
+    //             href="https://github.com/yezihaohao"
+    //             target="_blank"
+    //             rel="noopener noreferrer"
+    //           >
+    //             https://github.com/yezihaohao
+    //           </a>
+    //         </p>
+    //         <p>
+    //           博客地址：{" "}
+    //           <a
+    //             href="https://yezihaohao.github.io/"
+    //             target="_blank"
+    //             rel="noopener noreferrer"
+    //           >
+    //             https://yezihaohao.github.io/
+    //           </a>
+    //         </p>
+    //       </div>
+    //     ),
+    //     icon: <Icon type="smile-circle" style={{ color: "red" }} />,
+    //     duration: 0
+    //   });
+    //   localStorage.setItem("isFirst", JSON.stringify(true));
+    // };
+    // const isFirst = JSON.parse(localStorage.getItem("isFirst"));
+    // !isFirst && openNotification();
   }
   getClientWidth = () => {
     // 获取当前浏览器宽度并设置responsive管理响应式
@@ -102,8 +102,7 @@ class App extends Component {
               <Routes auth={auth} />
             </Content>
             <Footer style={{ textAlign: "center" }}>
-              React-Admin ©{new Date().getFullYear()} Created by
-              865470087@qq.com
+              React-Admin ©{new Date().getFullYear()} 
             </Footer>
           </Layout>
         </Layout>
